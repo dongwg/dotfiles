a5d6e7ce40f1e8aeca523f30a051df5109675d79
Weiguo Dong <weiguo.dong@cgg.com>
2015-05-13 16:41:32 +0100

    Create tags for winprod

--- linux/bin/gentags.sh
+++ linux/bin/gentags.sh
@@ -9,17 +9,24 @@ if [ -f /vega/xp/dev/wdong/tags/vega.tags ]; then
   rm -rf /vega/xp/dev/wdong/tags/vega.tags
 fi
 
-
 # Create tags for vega code with options required for omnicomplete in vim
 # Need to add "-l" to --c++-kinds (where 'l' is letter 'l') in order to 
 # make auto-completion work with local variables.
 ctags -f /vega/xp/dev/wdong/tags/vega.tags -R --c++-kinds=+pl --fields=+iaS --extra=+q /vega/xp/dev/wdong/vega
 
+if [ -f /vega/xp/dev/wdong/tags/winprod.tags ]; then
+  rm -rf /vega/xp/dev/wdong/tags/winprod.tags
+fi
 
+# Create tags for vega code with options required for omnicomplete in vim
+# Need to add "-l" to --c++-kinds (where 'l' is letter 'l') in order to 
+# make auto-completion work with local variables.
+ctags -f /vega/xp/dev/wdong/tags/winprod.tags -R --c++-kinds=+pl --fields=+iaS --extra=+q /vega/xp/dev/wdong/winprod_oldbuild
+
+# Python
 if [ -f /vega/xp/dev/wdong/tags/python.tags ]; then
   rm -rf /vega/xp/dev/wdong/tags/python.tags
 fi
-
 
 # Create tags for vega code with options required for omnicomplete in vim
 ctags -f /vega/xp/dev/wdong/tags/python.tags -R --python-kinds=-i /vega/xp/dev/wdong/python
