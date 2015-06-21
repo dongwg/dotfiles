8bfda0a898fe417dd5433c094142c3b7d8f72ae5
Weiguo Dong <dongwg@hotmail.com>
2015-06-21 12:20:09 +0100

    Fix issues with path to the files to be copied. Now wd_init can be run from any directory.

--- win/bin/wd_init.cmd
+++ win/bin/wd_init.cmd
@@ -1,19 +1,29 @@
 @echo off
 
+setlocal
+
+rem Find the root directory of WIN dotfiles which 
+rem is the parent directory of the batch file
+for %%i in ("%~dp0..") do set "wroot=%%~fi"
+
 rem get vundle.vim if it doesn't exist
-if not exist %userprofile%\vimfiles\bundle\Vundle.vim (
+if not exist "%userprofile%\vimfiles\bundle\Vundle.vim" (
   rem cd /d %userprofile%
   git clone "https://github.com/gmarik/Vundle.vim.git" "%userprofile%/vimfiles/bundle/Vundle.vim"
   )
 
 rem Copy vimrc
-echo copy "%~dp1etc\_vimrc" "%userprofile%"
-copy "%~dp1etc\_vimrc" "%userprofile%"
+if not exist "%userprofile%\_vimrc" (
+  echo copy "%wroot%\etc\_vimrc" "%userprofile%"
+  copy "%wroot%\etc\_vimrc" "%userprofile%"
+  )
 
 
 rem Copy bashrc
-echo copy "%~dp1etc\bashrc" "%userprofile%\.bashrc"
-copy "%~dp1etc\bashrc" "%userprofile%\.bashrc"
+if not exist "%userprofile%\.bashrc" (
+  echo copy "%wroot%\etc\bashrc" "%userprofile%\.bashrc"
+  copy "%wroot%\etc\bashrc" "%userprofile%\.bashrc"
+  )
 
 :END
 rem The end of the file
