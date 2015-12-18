a99e2827ffeee7fdc5a73c6914fe7137e8125642
Weiguo Dong <dongwg@hotmail.com>
2015-12-18 17:26:53 +0000

    Update the coniguration options to build php

--- linux/download/php-src/mybuild/mybuild.cmd
+++ linux/download/php-src/mybuild/mybuild.cmd
@@ -2,9 +2,11 @@
 
 make distclean
 
-git pull
+git pull origin master
 
-../configure --prefix=/usr/local --sysconfdir=/etc --localstatedir=/var --datadir=/usr/share/php --mandir=/usr/share/man --enable-fpm --with-fpm-user=www-data --with-fpm-group=www-data --with-config-file-path=/etc --with-zlib --enable-bcmath --with-bz2 --enable-calendar --disable-dba --with-gmp --enable-ftp --with-gettext --enable-mbstring --with-readline --disable-fileinfo --enable-maintainer-zts
+../buildconf --force
+
+../configure --prefix=/usr/local --sysconfdir=/etc --localstatedir=/var --datadir=/usr/share/php --mandir=/usr/share/man --enable-fpm --with-fpm-user=www-data --with-fpm-group=www-data --with-config-file-path=/etc --with-zlib --enable-bcmath --with-bz2 --enable-calendar --disable-dba --with-gmp --enable-ftp --with-gettext --enable-mbstring --with-readline --disable-fileinfo --enable-maintainer-zts --with-mysql --with-mysqli --with-qd --with-openssl --without-pear --enable-zip --with-curl --with-freetype-dir=/usr/local/freetype2 --enable-gd-native-ttf
 
 make
 
