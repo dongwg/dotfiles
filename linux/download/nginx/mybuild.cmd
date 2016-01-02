d0ac1ed5156afb5b85a959c5e12dc413764b7720
Weiguo Dong <dongwg@hotmail.com>
2016-01-02 18:09:43 +0000

    Update configuration for nginx to support http2

--- linux/download/nginx/mybuild.cmd
+++ linux/download/nginx/mybuild.cmd
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 hg pull
 hg update
 
 make clean
 
-auto/configure --user=www-data --group=www-data --with-cc-opt="-O3 -march=native -mtune=native" \--conf-path=/etc/nginx/nginx.conf --with-http_ssl_module --with-ipv6 --with-debug
+auto/configure --user=www-data --group=www-data --with-cc-opt="-O3 -march=native -mtune=native" \--conf-path=/etc/nginx/nginx.conf --with-http_ssl_module --with-ipv6 --with-debug --with-http_v2_module
 
 make
 
