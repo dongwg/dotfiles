89d6a70b2f1368e2ca2a31f9c582cfbb7fe3631e
taohe <taohe@hotmail.com>
2016-12-26 17:34:40 +0000

    Add some addition configure options to nginx according to the document

--- linux/download/nginx/mybuild.cmd
+++ linux/download/nginx/mybuild.cmd
@@ -8,7 +8,8 @@ hg update
 
 # Configure
 auto/configure --user=www-data --group=www-data --with-cc-opt="-O3 -march=native -mtune=native" \
---conf-path=/etc/nginx/nginx.conf --with-http_ssl_module --with-ipv6 --with-debug --with-http_v2_module
+--conf-path=/etc/nginx/nginx.conf --with-http_ssl_module --with-ipv6 --with-debug --with-http_v2_module \
+--with-stream --with-mail=dynamic 
 
 # Build
 make
