949fd69834e4b0f4f212e7a727b4af2070aa295c
Weiguo Dong <taohe@hotmail.com>
2021-04-25 17:16:37 +0100

    Modify backupgit.sh to run on dev-vm1 or dev-db11 and backup to the diskstation

--- linux/bin/backupgit.sh
+++ linux/bin/backupgit.sh
@@ -2,8 +2,8 @@
 
 PATH=/opt/bin:/opt/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-projects_dir=/mnt/disk1/share/repos
-backup_base_dir=/mnt/disk1/ds213_share/repos/backup 
+projects_dir=/s0/repos
+backup_base_dir=/mnt/ds213/repos/backup 
 
 # For some reason, the file system wouldn't be monted after the linkstation is rebooted
 # So, try to mount the file system firstly
@@ -17,22 +17,35 @@ log_file=~/backupgit.log
 # Reset the log file
 : > ${log_file}
 
+today=$(date +%Y%m%d)
+#yesterday=$(date -D "%s" -d $(( $(/bin/date +%s ) - 86400 ))  +%Y%m%d)
+yesterday=$(date -d "yesterday"  +%Y%m%d)
+
 pushd $projects_dir 
 
 git_status=0
 
 echo "[Create git bundles]" >> ${log_file}
-for d in `find $projects_dir -type d -name \*.git`; do 
+###for d in `find $projects_dir -type d -name \*.git`; do 
+###Exclude Calibre
+for d in `find $projects_dir -type d -name \*.git -not -path "*ebooks*"`; do 
 	cd $d 
 	repo=${d##*/}
-	repo=${repo%.*}_$(date +%Y%m%d) 
-        echo "  cd $PWD" >> ${log_file} 2>&1
-	echo "  git bundle create ${backup_base_dir}/${repo}.bundle --all" >> ${log_file} 2>&1
-        echo "  " >> ${log_file} 2>&1
-	git bundle create ${backup_base_dir}/${repo}.bundle --all >> ${log_file} 2>&1
-	if [ $? -ne 0 ]; then
+	repo=${repo%.*}_${today} 
+	bundle=${backup_base_dir}/${repo}.bundle
+	if [ -e ${bundle} ];
+	then
+	    echo " ${bundle} already exists! Skip backing up ${repo}" >> ${log_file} 2>&1
+	else
+	    echo " Back up ${repo}" >> ${log_file} 2>&1
+            echo "  cd $PWD" >> ${log_file} 2>&1
+	    echo "  git bundle create ${backup_base_dir}/${repo}.bundle --all" >> ${log_file} 2>&1
+            echo "  " >> ${log_file} 2>&1
+	    git bundle create ${backup_base_dir}/${repo}.bundle --all >> ${log_file} 2>&1
+	    if [ $? -ne 0 ]; then
 		git_status = $? 
 		echo "  $* failed with exit code $?" >> ${log_file}
+	    fi
 	fi
 done 
 popd 
@@ -42,24 +55,20 @@ popd
 if [ ${git_status} -eq 0 ]; then
 	echo " " >> ${log_file}
 	echo "[Remove bundles older than 2 days]" >> ${log_file}
-        find /mnt/disk1/ds213_share/repos/backup/ -name "*.bundle" -mtime +1 -exec echo "  rm -f {}" >> ${log_file} \;	
-	find /mnt/disk1/ds213_share/repos/backup/ -name "*.bundle" -mtime +1 -exec rm -f {} >> ${log_file} 2>&1 \;
+        find $backup_base_dir -name "*.bundle" -mtime +1 -exec echo "  rm -f {}" >> ${log_file} \;	
+	find $backup_base_dir -name "*.bundle" -mtime +1 -exec rm -f {} >> ${log_file} 2>&1 \;
 	
 	echo " " >> ${log_file}
-	echo "[Update the default CalibreLibrary.bundle]" >> ${log_file}
-        echo "  Use ${backup_base_dir}/CalibreLibrary_$(date +%Y%m%d).bundle" >> ${log_file}
-	rm -rf ${backup_base_dir}/CalibreLibrary.bundle
-	cp ${backup_base_dir}/CalibreLibrary_$(date +%Y%m%d).bundle ${backup_base_dir}/CalibreLibrary.bundle
 fi
 
 # Send the log to my email box
-mail_inp=~/mailbody.txt
+#mail_inp=~/mailbody.txt
 # Reset the mail content firstly
-: > ${mail_inp}
+#: > ${mail_inp}
 # Compose the email, mail-header.txt is expected in the HOME of root
-cat ~/mail-header.txt > ${mail_inp}
-cat ${log_file} >> ${mail_inp}
+#cat ~/mail-header.txt > ${mail_inp}
+#cat ${log_file} >> ${mail_inp}
 # send the email using ssmtp
-ssmtp dongwg@hotmail.com < ${mail_inp}
+#ssmtp dongwg@hotmail.com < ${mail_inp}
  
 #
